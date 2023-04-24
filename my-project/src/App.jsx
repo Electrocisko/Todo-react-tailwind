06c7be21170a2bc4e533c390d05a4ad1c918538c
Francisco Zuidwijk <electrocisko@gmail.com>
2023-04-24 15:44:21 -0300

    Dark mode y localstrorage

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,85 +1,87 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
 import TodoCreate from "./components/TodoCreate";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
- 
 
-const initialState= [
-  {id:1,  title: "Funca esto??", completed: true},
-  {id:2,  title: "Funca ", completed: false},
-  {id:3,  title: "Funca si??", completed: true},
-  {id:4,  title: "Funca no??", completed: false},
-  {id:5,  title: "Funca Funky??", completed: false}
-]
+const initialState = JSON.parse(localStorage.getItem("todos","todos")) || [];
 
 const App = () => {
-
   const [todos, setTodos] = useState(initialState);
 
   const [filter, setFilter] = useState("all");
+
+  useEffect(() => { 
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos]);
 
   const createTodo = (title) => {
     const newTodo = {
       id: Date.now(),
       title: title,
-      completed: false
-    }
-    setTodos([...todos, newTodo])
-  }
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
 
   const updatedTodo = (id) => {
-     const list = todos.map((item) =>{
-          if(item.id === id) {
-            item.completed = !item.completed
-          }
-          return item
-        })
-        setTodos(list);
-  }
+    const list = todos.map((item) => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    setTodos(list);
+  };
 
   const removeTodo = (id) => {
-    let list = todos.filter((todo) => todo.id !== id )
-    setTodos(list)
-  }
+    let list = todos.filter((todo) => todo.id !== id);
+    setTodos(list);
+  };
 
   const computedItemsLeft = () => {
     return todos.filter((todo) => !todo.completed).length;
-  }
+  };
 
   const clearComplete = () => {
-    let list = todos.filter((todo) => todo.completed === false )
-    setTodos(list)
-  }
+    let list = todos.filter((todo) => todo.completed === false);
+    setTodos(list);
+  };
 
   const filterTodo = () => {
     if (filter === "active") {
-      return ( todos.filter((todo) =>  todo.completed === false))
+      return todos.filter((todo) => todo.completed === false);
     }
     if (filter === "completed") {
-      return ( todos.filter((todo) =>  todo.completed === true))
+      return todos.filter((todo) => todo.completed === true);
     }
-   return todos;
-  }
+    return todos;
+  };
 
   const changeFilter = (state) => {
-   setFilter(state)
-  }
+    setFilter(state);
+  };
 
   return (
-    <div className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] ">
+    <div className=" min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] ">
       <Header />
 
       <main className="container mx-auto mt-8 px-4  dark:bg-gray-900">
         <TodoCreate createTodo={createTodo} />
 
-        <TodoList todos={ filterTodo(todos)} updatedTodo={updatedTodo} removeTodo={removeTodo}/>
-        
-        <TodoComputed computedItemsLeft={computedItemsLeft()} clearComplete={clearComplete}/>
+        <TodoList
+          todos={filterTodo(todos)}
+          updatedTodo={updatedTodo}
+          removeTodo={removeTodo}
+        />
 
-        <TodoFilter changeFilter={changeFilter}  filter={filter}/>
-    
+        <TodoComputed
+          computedItemsLeft={computedItemsLeft()}
+          clearComplete={clearComplete}
+        />
+
+        <TodoFilter changeFilter={changeFilter} filter={filter} />
       </main>
 
       <footer className="mt-4 text-center">Drag and Drop List</footer>
--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import MoonIcon from "./icons/MoonIcon";
+import SunIcon from "./icons/SunIcon";
 
+ let initialStateTheme = localStorage.getItem("theme") === "dark";
 
 const Header = () => {
+
+  const [darkMode, setDarkMode] = useState(initialStateTheme);
+
+  useEffect(() => {
+    if(darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme","dark")
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme","light")
+    }
+  },[darkMode]);
+
+  const handleClickToggleTheme = () => {
+   setDarkMode(!darkMode);
+  }
+
     return(
         <header className="container mx-auto px-4 pt-8">
         <div className="flex justify-between ">
           <h1 className="text-3xl font-semibold  text-white ">T O D O</h1>
-          <button>
-            <MoonIcon/>
+          <button onClick={handleClickToggleTheme}>
+            { darkMode ? <SunIcon/> : <MoonIcon/> }
           </button>
         </div>
       </header>
