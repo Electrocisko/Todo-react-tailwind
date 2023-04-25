c907d2671c901b2f194527337b27d727584bab3a
Francisco Zuidwijk <electrocisko@gmail.com>
2023-04-25 07:57:35 -0300

    filter

--- my-project/src/components/TodoFilter.jsx
+++ my-project/src/components/TodoFilter.jsx
@@ -1,17 +1,40 @@
-
-const TodoFilter = ({changeFilter, filter}) => {
-    return(
-        <section className="container mx-auto mt-4 rounded-md  py-4 ">
-        <div className="flex justify-center gap-4 bg-white p-4  dark:bg-gray-800">
-          <button onClick={ () =>  changeFilter("all")}
-           className={` dark:text-gray-300${filter === "all" ? "text-blue-500 hover:text-blue-500" :"text-black hover:text-blue-500"}`}>
-            
-            All</button>
-          <button onClick={ () =>  changeFilter("active")} className="hover:text-blue-600  dark:text-gray-300">Active</button>
-          <button onClick={ () =>  changeFilter("completed")}  className="hover:text-blue-600  dark:text-gray-300">Completed</button>
-        </div>
-      </section>
-    )
-}
+const TodoFilter = ({ changeFilter, filter }) => {
+  return (
+    <section className="container mx-auto mt-4 rounded-md  py-4 ">
+      <div className="flex justify-center gap-4 bg-white p-4  dark:bg-gray-800">
+        <button
+          onClick={() => changeFilter("all")}
+          className={`${
+            filter === "all"
+                ? "text-blue-500 hover:text-gray-400"
+                : "text-gray-400 hover:text-blue-500"
+        }`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => changeFilter("active")}
+          className={`${
+            filter === "active"
+                ? "text-blue-500 hover:text-gray-400"
+                : "text-gray-400 hover:text-blue-500"
+        }`}
+        >
+          Active
+        </button>
+        <button
+          onClick={() => changeFilter("completed")}
+          className={`${
+            filter === "completed"
+                ? "text-blue-500 hover:text-gray-400"
+                : "text-gray-400 hover:text-blue-500"
+        }`}
+        >
+          Completed
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default TodoFilter;
